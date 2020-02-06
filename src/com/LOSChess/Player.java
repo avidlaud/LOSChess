750c8296cc327db53bfa0c3fbb2bb90446f9cb59
avidlaud <avidlaud@gmail.com>
2020-02-05 19:49:53 -0500

    Implement constructor and pieces list

--- src/com/LOSChess/Player.java
+++ src/com/LOSChess/Player.java
@@ -1,8 +1,12 @@
 package com.LOSChess;
 
+import java.util.ArrayList;
+
 public class Player {
 
     boolean isWhite;
+
+    public ArrayList<Piece> pieces;
 
     public boolean isWhite() {
         return isWhite;
@@ -10,6 +14,24 @@ public class Player {
 
     public boolean isBlack() {
         return !isWhite;
+    }
+
+    public Player(Board board, boolean isWhite) {
+        int[] pos = {0, 0};
+        for(int file = 1; file <= 8; file++) {
+            for (int rank = 1; rank <= 8; rank++) {
+                pos[0] = file;
+                pos[1] = rank;
+                if(board.checkPos(pos) != null) {
+                    if (board.checkPos(pos).getPlayer().isWhite() && isWhite) { //White pieces
+                        pieces.add(board.checkPos(pos));
+                    } else if (board.checkPos(pos).getPlayer().isBlack() && !isWhite) { //Black pieces
+                        pieces.add(board.checkPos(pos));
+                    }
+                }
+            }
+        }
+        this.isWhite = isWhite;
     }
 
 }
